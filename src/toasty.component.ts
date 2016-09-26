721dd0379052e4e67bee0fd0cd30b56cc2b92d4e
muetzerich <muetzerich@users.noreply.github.com>
2016-09-26 16:34:17 +0200

    Update toasty.component.ts
    delete unnecessary moduleId parameter

--- src/toasty.component.ts
+++ src/toasty.component.ts
@@ -11,7 +11,6 @@ import { ToastyService, ToastData, ToastyConfig } from './toasty.service';
  * Toasty is container for Toast components
  */
 @Component({
-  moduleId: module.id.toString(),
   selector: 'ng2-toasty',
   template: `
     <div id="toasty" [ngClass]="[position]">
