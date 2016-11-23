7d7c0a837991f5bf7e2e448944d9e285d20725f1
Sergey Akopkokhyants <sergey@bitx.co>
2016-11-23 14:53:49 +0200

    perf(): Build pipeline moved to Webpack. Added AOT support.

--- index.ts
+++ index.ts
@@ -2,10 +2,7 @@
 // This project is licensed under the terms of the MIT license.
 // https://github.com/akserg/ng2-toasty
 
-'use strict';
-
 import { NgModule, ModuleWithProviders } from "@angular/core";
-import { CommonModule } from "@angular/common";
 
 export * from './src/toasty.service';
 export * from './src/toasty.component';
@@ -14,13 +11,7 @@ import { ToastyComponent } from './src/toasty.component';
 import { ToastComponent } from './src/toast.component';
 import { ToastyConfig, ToastyService } from './src/toasty.service';
 
-export default {
-  providers : [ToastyConfig, ToastyService],
-  directives: [ToastyComponent, ToastComponent]
-};
-
 @NgModule({
-  imports: [CommonModule],
   declarations: [ToastComponent, ToastyComponent],
   exports : [ToastComponent, ToastyComponent]
 })
