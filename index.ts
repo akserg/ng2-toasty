b47b819b9389c1c282bf271c33871ef314ac1c87
Julien Barbedette <barbedette.julien@gmail.com>
2017-02-28 13:29:45 -0600

    fix(aot): export toast.component

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import { CommonModule } from '@angular/common';
 
 export * from './src/toasty.service';
 export * from './src/toasty.component';
+export * from './src/toast.component';
 
 import { ToastyComponent } from './src/toasty.component';
 import { ToastComponent } from './src/toast.component';
