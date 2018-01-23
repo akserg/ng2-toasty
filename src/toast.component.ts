aecc82486446ae4cb5e2d8e56e01fa53f16aedda
Anand Rockzz <anandchakru.forum@gmail.com>
2018-01-23 01:00:13 -0800

    anim-bar
    animating bar inside each toast indicating when the toast will self-destruct [Imgur](https://i.imgur.com/TJL3CIW.gifv)

--- src/toast.component.ts
+++ src/toast.component.ts
@@ -13,6 +13,7 @@ import { ToastData } from './toasty.service';
   selector: 'ng2-toast',
   template: `
         <div class="toast" [ngClass]="[toast.type, toast.theme]">
+            <div class="time-bar" [ngStyle]="timeBarAnim()">
             <div *ngIf="toast.showClose" class="close-button" (click)="close($event)"></div>
             <div *ngIf="toast.title || toast.msg" class="toast-text">
                 <span *ngIf="toast.title" class="toast-title" [innerHTML]="toast.title | safeHtml"></span>
@@ -26,6 +27,15 @@ export class ToastComponent {
   @Input() toast: ToastData;
   @Output('closeToast') closeToastEvent = new EventEmitter();
 
+  /**
+   * Returns Animation Style for time-bar
+   *
+   */
+  timeBarAnim() {
+    let lef = this.toast.timeout + 'ms linear 0s 1 anim-time-bar';
+    return { 'animation': + lef, '-webkit-animation': lef, '-moz-animation': lef };
+  }
+  
   /**
    * Event handler invokes when user clicks on close button.
    * This method emit new event into ToastyContainer to close it.
